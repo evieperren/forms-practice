2e5c3a4250ccbfd4a7c78b069a1da43dc7b74587
Evie Perren <evie.perren@simplyhealth.co.uk>
2020-10-23 09:37:04 +0100

    using judits changes

--- index.js
+++ index.js
@@ -1,69 +1,94 @@
-const secondDropdown = document.querySelector('#category')
-const allContentBlocks = document.querySelectorAll('.toggle-content')
+var filterDropdown = document.getElementById("filter");
+var categoryDropdown = document.getElementById("category");
+var selectedSection = document.getElementById("selected-content");
+var showFormBtn = document.getElementById("show-form-btn");
+var form = document.getElementById("form");
+var selectedOptionDropdown = document.getElementById("selected-option");
+var filterOption = document.querySelectorAll("[data-filter]");
+var categoryOption = document.querySelectorAll("[data-category]");
+var selectedOption = "";
+var selectedContent = "";
 
-var show = function (elem) {
-  elem.classList.add('is-visible');
-  elem.classList.remove('is-hidden');
-};
-var hide = function (elem) {
-  elem.classList.add('is-hidden');
-  elem.classList.remove('is-visible');
-};
+function filterCategory() {
+  resetContent();
+  resetCategories();
+  resetSelectedOption();
 
-function displayContentAfterInitialDropdown(){
-  const select = document.querySelector('#filter')
-  hide(secondDropdown)
+  // set filter
+  selectedOption = filterDropdown.value;
 
-  for( let i= 0; i < allContentBlocks.length; i++){
-    hide(allContentBlocks[i])
-  }
-  if(select)[
-    select.addEventListener('change', function(event) {
-      const selectedItem = event.target[event.target.selectedIndex]
-      selectedItem.selected = true
-  
-      const selectedAttr = selectedItem.getAttribute('data-filter');
-  
-      const dropdownItems = document.querySelectorAll("[data-category]")
-      for(let i = 0; i < dropdownItems.length; i++){
-        hide(dropdownItems[i])
+  // reveal categories dropdown
+  categoryDropdown.classList.add("is-visible");
+
+  // show only filtered categories
+  for (var i in categoryOption)
+    if (categoryOption.hasOwnProperty(i)) {
+      categoryOption[i].classList.add("is-hidden");
+      if (categoryOption[i].getAttribute("data-category") !== selectedOption) {
+        categoryOption[i].classList.add("is-hidden");
+      } else {
+        categoryOption[i].classList.remove("is-hidden");
       }
-      filterSecondDropdown(selectedAttr)
-    })
-  ]
+    }
 }
 
-function filterSecondDropdown(selectedAttr){
-  const correspondingDropdownEntries = document.querySelectorAll("[data-category="+ selectedAttr +"]")
-  for(let k = 0; k < correspondingDropdownEntries.length; k++){
-    show(correspondingDropdownEntries[k])
+function showContent() {
+  selectedContent = categoryDropdown.value;
+
+  resetContent();
+
+  document.getElementById(selectedContent).classList.add("is-visible");
+  showFormBtn.classList.add("is-visible");
+
+  setSelectedOption();
+}
+
+showFormBtn.addEventListener("click", function (event) {
+  setSelectedOption();
+  form.classList.add("is-visible");
+});
+
+function setSelectedOption() {
+  var opts = selectedOptionDropdown.options;
+  //selectedContent = categoryDropdown.value;
+
+  for (var i = 0; i < opts.length; i++) {
+    if (opts[i].getAttribute("data-option") == selectedContent) {
+      var found = opts[i];
+      found.selected = true;
+      break;
+    }
   }
-  show(secondDropdown)
 }
 
-function displayContentAfterSecondDropdown(){
-  const select = document.querySelector('#category')
-  if(select){
-    select.addEventListener('change', function(event) {
-      const selectedItem = event.target[event.target.selectedIndex]
-      selectedItem.selected = true 
-      const selectedAttr = event.target[event.target.selectedIndex].value;
-  
-      if(selectedAttr !== ''){
-        const correspondingDropdownEntry = document.querySelector('#' + selectedAttr)
-        for( let i= 0; i < allContentBlocks.length; i++){
-          hide(allContentBlocks[i])
-        }
-        show(correspondingDropdownEntry)
-        show(correspondingDropdownEntry.parentElement)
-      }
-    })
+function hasClass(element, className) {
+  return (" " + element.className + " ").indexOf(" " + className + " ") > -1;
+}
+
+function resetCategories() {
+  categoryDropdown.selectedIndex = 0;
+}
+
+function resetContent() {
+  var y = selectedSection.getElementsByClassName("is-visible");
+  var i;
+  if (y.length) {
+    for (i = 0; i < y.length; i++) {
+      y[i].classList.remove("is-visible");
+    }
+  }
+
+  var classes = ["is-visible"];
+
+  for (var i = 0, j = classes.length; i < j; i++) {
+    if (hasClass(showFormBtn, classes[i])) {
+      showFormBtn.classList.remove("is-visible");
+      break;
+    }
   }
 }
-displayContentAfterInitialDropdown()
-displayContentAfterSecondDropdown()
 
-
-// use 'selected' attribute 
-// hide secondary dropdown and content block on second click 
-// 
+function resetSelectedOption() {
+  form.classList.remove("is-visible");
+  selectedOptionDropdown.selectedIndex = 0;
+}
